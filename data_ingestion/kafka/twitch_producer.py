a3844818e42c5643295036c4cf0adfe375affc6d
AdriaPares <adriaparespalacin@gmail.com>
2019-06-26 19:58:23 -0400

    Change to read from s3.

--- data_ingestion/kafka/twitch_producer.py
+++ data_ingestion/kafka/twitch_producer.py
@@ -9,11 +9,8 @@ import datetime as dt
 # this needs a timestamp
 # and also a try/except for JSON DECODE ERROR (if the topic gets thrown bad stuff it will stop the consumer)
 
-# s3 = boto3.resource('s3')
-#
-# content_object = s3.Object('insight-api-dumps', 'twitch_dump/twitch_2mgovercsquared.json')
-# file_content = content_object.get()['Body'].read().decode('utf-8')
-# json_content = json.loads(file_content)
+s3 = boto3.resource('s3')
+
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: json.dumps(x).encode('utf-8'))
@@ -25,8 +22,13 @@ initial_timestamp = '2019-06-25_13-21-33_'
 time_format = '%Y-%m-%d_%H-%M-%S_'
 second_counter = 0
 while True:
+
     current_timestamp = dt.datetime.strftime(dt.datetime.strptime(initial_timestamp, time_format) +
                                              dt.timedelta(seconds=second_counter), time_format)
-    with open(folder_path + current_timestamp + platform + streamer_name + '.json') as f:
-        json_content = json.load(f)
+
+    content_object = s3.Object('insight-api-dumps', current_timestamp + platform + streamer_name + '.json')
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    # with open(folder_path + current_timestamp + platform + streamer_name + '.json') as f:
+        # json_content = json.load(f)
     producer.send('twitch-topic', value={current_timestamp + streamer_name: json_content['total']})
