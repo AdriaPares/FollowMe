1248c3db8a8ecee955b0fbab5d2b0620fd30ccf9
AdriaPares <adriaparespalacin@gmail.com>
2019-07-03 06:53:45 -0400

    Add FollowMe server with Dash application placeholders.

---
+++ flask/FollowMe/application/dash_application/layout.py
@@ -0,0 +1,23 @@
+html_layout = '''<!DOCTYPE html>
+                    <html>
+                        <head>
+                            {%metas%}
+                            <title>{%title%}</title>
+                            {%favicon%}
+                            {%css%}
+                        </head>
+                        <body>
+                            <nav>
+                              <a href="/"><i class="fas fa-home"></i> FollowMe</a>
+                              <a href="/followers/"><i class="fas fa-hashtag"></i> Follower Count</a>
+                              <a href="/categories/"><i class="fas fa-medal"></i> Categories</a>
+                              <a href="/trends/"><i class="fas fa-chart-line"></i> Trends</a>
+                            </nav>
+                            {%app_entry%}
+                            <footer>
+                                {%config%}
+                                {%scripts%}
+                                {%renderer%}
+                            </footer>
+                        </body>
+                    </html>'''
