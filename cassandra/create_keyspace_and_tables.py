d7febb5cf7f637b8167e6b0c19bb7c9e358e3682
AdriaPares <adriaparespalacin@gmail.com>
2019-07-04 16:09:20 -0400

    Added unified tables, removed trends.

--- cassandra/create_keyspace_and_tables.py
+++ cassandra/create_keyspace_and_tables.py
@@ -1,54 +1,65 @@
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, Session
 
-cassandra_cluster = Cluster(['10.0.0.5', '10.0.0.7', '10.0.0.12', '10.0.0.19'])
-cassandra_session = cassandra_cluster.connect('')
 
-create_table_query = 'create table if not exists insight.'
-create_ledger_columns_flask = ' (streamer text, timestamp text, follower_count int, primary key (streamer, timestamp)) '
-create_ledger_columns_spark = ' (streamer text, timestamp text, follower_count int, primary key (timestamp, streamer)) '
-create_trend_columns = ' (streamer text, timestamp text, trend int, primary key (streamer, timestamp)) '
-platforms = ['twitch', 'twitter', 'youtube']
-# time_frames = {'_live ': 'with default_time_to_live=120;',
-#                '_minute ': 'with default_time_to_live=7200;',
-#                '_hour ': 'with default_time_to_live=172800;',
-#                '_day ': ';'}
-time_frames = ['_live', '_minute', '_hour', '_day']
+# Returns List of tables to create
+def get_cassandra_tables() -> list:
+    tables_to_create = []
+    create_table = 'create table if not exists insight.'
+    partition_by_streamer = ' (streamer text, timestamp text, follower_count int, primary key (streamer, timestamp)) '
+    partition_by_timestamp = ' (streamer text, timestamp text, follower_count int, primary key (timestamp, streamer)) '
+    platforms = ['twitch', 'twitter', 'youtube']
+    time_frames = ['_live', '_minute', '_hour', '_day']
+    for platform in platforms:
+        for time in time_frames:
+            # streamer follower by platform and time frame
+            tables_to_create.append(create_table + platform + time + '_by_streamer' + partition_by_streamer + ' ;')
+            tables_to_create.append(create_table + platform + time + '_by_timestamp' + partition_by_timestamp + ' ;')
+
+    # Unified streamer data
+    tables_to_create.append('create table if not exists insight.unified_followers '
+                            '(streamer text, timestamp text, youtube_count int, twitter_count int, twitch_count int, '
+                            'total_count bigint, '
+                            'primary key (timestamp, streamer));')
+
+    # Aggregations
+    tables_to_create.append('create table if not exists insight.language_aggregation '
+                            '(language text, timestamp text, total_count bigint, primary key (timestamp, language));')
+    tables_to_create.append('create table if not exists insight.game_aggregation '
+                            '(game text, timestamp text, total_count bigint, primary key (timestamp, game));')
+    tables_to_create.append('create table if not exists insight.genre_aggregation '
+                            '(genre text, timestamp text, total_count bigint, primary key (timestamp, genre));')
+    tables_to_create.append('create table if not exists insight.console_aggregation '
+                            '(console text, timestamp text, total_count bigint, primary key (timestamp, console));')
+    # Accounts info
+    tables_to_create.append('create table if not exists insight.accounts (streamer text primary key, '
+                            'language text, game text);')
+    # Games info
+    tables_to_create.append('create table if not exists insight.games '
+                            '(game text primary key, genre text, console text);')
+
+    return tables_to_create
+
 
 # Create Keyspace
-cassandra_session.execute("CREATE KEYSPACE IF NOT EXISTS insight "
-                          "WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};")
+def create_keyspace(session: Session, keyspace: str = 'insight') -> None:
+    session.execute("CREATE KEYSPACE IF NOT EXISTS " + keyspace +
+                    " WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 3};")
 
-for platform in platforms:
-    # Create Trend tables
-    cassandra_session.execute(create_table_query + platform + '_trend' + create_trend_columns)
 
-    # Create temporal daily tables NOT NEEDED AFTER DUPLICATION OF DATA
-    # cassandra_session.execute(create_table_query + platform + '_day_temp_table ' +
-    #                           create_ledger_columns_flask + time_frames['_hour'])
+# Create Tables
+def create_tables(tables: list, session: Session) -> None:
+    for table in tables:
+        session.execute(table)
 
-    # for time, time_to_live in time_frames.items():
-    for time in time_frames:
-        # Create Record tables
-        # cassandra_session.execute(create_table_query + platform + time + create_ledger_columns_flask + time_to_live)
-        cassandra_session.execute(create_table_query + platform + time + create_ledger_columns_flask + ' ;')
-        cassandra_session.execute(create_table_query + platform + time + '_spark' + create_ledger_columns_spark + ' ;')
 
-# Create Account metadata tables
+def main():
+    cassandra_cluster = Cluster(['10.0.0.5', '10.0.0.7', '10.0.0.12', '10.0.0.19'])
+    cassandra_session = cassandra_cluster.connect('')
+    cassandra_tables = get_cassandra_tables()
+    create_keyspace(cassandra_session)
+    create_tables(cassandra_tables, cassandra_session)
+    cassandra_cluster.shutdown()
 
-# Accounts
-cassandra_session.execute('create table if not exists insight.accounts (streamer text primary key, '
-                          'language text, game text);')
-# Games
-cassandra_session.execute('create table if not exists insight.games (game text primary key, genre text, console text);')
 
-# Aggregations
-cassandra_session.execute('create table if not exists insight.language_aggregation '
-                          '(language text, timestamp text, total_count bigint, primary key (timestamp, language));')
-cassandra_session.execute('create table if not exists insight.game_aggregation '
-                          '(game text, timestamp text, total_count bigint, primary key (timestamp, game));')
-cassandra_session.execute('create table if not exists insight.genre_aggregation '
-                          '(genre text, timestamp text, total_count bigint, primary key (timestamp, genre));')
-cassandra_session.execute('create table if not exists insight.console_aggregation '
-                          '(console text, timestamp text, total_count bigint, primary key (timestamp, console));')
-
-cassandra_cluster.shutdown()
+if __name__ == '__main__':
+    main()
