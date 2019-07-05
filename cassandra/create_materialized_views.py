f80c5b43f5326b0667650802e8be52cbf619ea40
AdriaPares <adriaparespalacin@gmail.com>
2019-07-05 10:38:53 -0400

    Fixed typos in view creation.

--- cassandra/create_materialized_views.py
+++ cassandra/create_materialized_views.py
@@ -3,17 +3,18 @@ import datetime as dt
 
 
 # Views are partitioned by timestamp and clustered by streamer
-def get_views(day: str = dt.datetime.today().strftime('%Y-%m-%d'),
+def get_views(day: str = (dt.datetime.today()-dt.timedelta(days=1)).strftime('%Y-%m-%d'),
               last_month: str = (dt.datetime.today() - dt.timedelta(weeks=4)).strftime('%Y-%m-%d')) -> list:
     views = []
     tables_to_view = ['twitch_live', 'twitter_live', 'youtube_live', 'unified_minute', 'unified_hour']
     for table_to_view in tables_to_view:
-        views.append("create MATERIALIZED VIEW " + table_to_view + "_view as select * from " + table_to_view
-                     + " where streamer is not null and timestamp > '" + day + "primary key (timestamp, streamer) ;")
+        views.append("create MATERIALIZED VIEW if not exists " + table_to_view + "_view as select * from "
+                     + table_to_view + " where streamer is not null and timestamp > '"
+                     + day + "' primary key (timestamp, streamer) ;")
 
     # Unified day requires the last month for plotting in Dash
-    views.append("create MATERIALIZED VIEW unified_day_view as select * from unified_day where streamer is not null "
-                 "and timestamp > '" + last_month + "primary key (timestamp, streamer) ;")
+    views.append("create MATERIALIZED VIEW if not exists unified_day_view as select * from unified_day where streamer "
+                 "is not null and timestamp > '" + last_month + "' primary key (timestamp, streamer) ;")
     return views
 
 
