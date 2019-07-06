2eab1c239e3bfd064a274c4f849a6e9ef210f42b
AdriaPares <adriaparespalacin@gmail.com>
2019-07-05 23:16:04 -0400

    Modified spark_*.sh to account for new Python scripts in Spark Master.

--- airflow/spark_hour_to_day.sh
+++ airflow/spark_hour_to_day.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 export SPARK_HOME=/usr/local/spark
-python3 ~/cassandra_jobs/youtube_hour_to_day.py
-python3 ~/cassandra_jobs/twitch_hour_to_day.py
-python3 ~/cassandra_jobs/twitter_hour_to_day.py
+python3 ~/cassandra_jobs/spark_hour_to_day.py
--- airflow/spark_live_to_minute.sh
+++ airflow/spark_live_to_minute.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 export SPARK_HOME=/usr/local/spark
-python3 ~/cassandra_jobs/youtube_live_to_minute.py
-python3 ~/cassandra_jobs/twitch_live_to_minute.py
-python3 ~/cassandra_jobs/twitter_live_to_minute.py
+python3 ~/cassandra_jobs/spark_live_to_minute.py
